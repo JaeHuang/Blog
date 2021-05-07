af0684c83f7d409a2196f1e51968c1b99a0c29be
Jae <jae.huang@nielsen.com>
2021-05-07 17:44:43 +0800

    Add register info to db and validate existed username, email

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bacf29a9006f30d1bf7ad28fab8593c5'
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import route
